b6970b7dbc8ae967c593365bd6657048b25ced1f
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-06 14:40:28 -0500

    created GET /api/v1/movies/:id API method, associated query/ test

--- test/routes.movies.test.js
+++ test/routes.movies.test.js
@@ -16,10 +16,6 @@ describe('routes : movies', () => {
     .then(() => { return knex.seed.run(); });
   });
 
-  afterEach(() => {
-    return knex.migrate.rollback();
-  });
-
   describe('GET /api/v1/movies', () => {
     it('should return all movies', (done) => {
       chai.request(server)
@@ -39,6 +35,30 @@ describe('routes : movies', () => {
         res.body.data.length.should.eql(3);
         // the first object in the data array should
         // have the right keys
+        res.body.data[0].should.include.keys(
+          'id', 'name', 'genre', 'rating', 'explicit'
+        );
+        done();
+      });
+    });
+  });
+
+  describe('GET /api/v1/movies/:id', () => {
+    it('should respond with a single movie', (done) => {
+      chai.request(server)
+      .get('/api/v1/movies/1')
+      .end((err, res) => {
+        // there should be no errors
+        should.not.exist(err);
+        // there should be a 200 status code
+        res.status.should.equal(200);
+        // the response should be JSON
+        res.type.should.equal('application/json');
+        // the JSON response body should have a
+        // key-value pair of {"status": "success"}
+        res.body.status.should.eql('success');
+        // the JSON response body should have a
+        // key-value pair of {"data": 1 movie object}
         res.body.data[0].should.include.keys(
           'id', 'name', 'genre', 'rating', 'explicit'
         );
