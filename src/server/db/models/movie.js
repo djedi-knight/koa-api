54b13557167dc6b05ac4ef74cdd9e56df1b13bd1
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-07 14:13:28 -0500

    added bookshelf-schema module/ added schema to Movies model

--- src/server/db/models/movie.js
+++ src/server/db/models/movie.js
@@ -1,6 +1,20 @@
+const Schema = require('bookshelf-schema');
+const Fields = require('bookshelf-schema/lib/fields');
+
+// load bookshelf
 const knex = require('../connection');
 const bookshelf = require('bookshelf')(knex);
 
+// load bookshelf Schema plugin
+bookshelf.plugin(Schema());
+
 module.exports = bookshelf.Model.extend({
   tableName: 'movies'
+}, {
+  schema: [
+    Fields.StringField('name', {required: true}),
+    Fields.StringField('genre', {required: true}),
+    Fields.IntField('rating', {required: true, natural: true}),
+    Fields.BooleanField('explicit', {required: true})
+  ]
 });
